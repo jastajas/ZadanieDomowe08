443fe0c55da34580bb80f8ab13245497ea70f01b
jackS <jastajas@gamil.com>
2018-01-25 16:54:39 +0100

    Klasa TestTriangle (main) - ver.2 dodanie spradzenia możliwości tworzenia trójkąta

--- src/TestTriangle.java
+++ src/TestTriangle.java
@@ -1,15 +1,28 @@
+import java.util.Scanner;
+
 public class TestTriangle {
     public static void main(String[] args) {
 
-        Triangle triangle = new Triangle(5,4,3);
-        TriangleCalc triangleCalc = new TriangleCalc();
+        Scanner sc = new Scanner(System.in);
 
-        if (triangleCalc.isRightTriangle(triangle)){
-            System.out.println("Trójkąt jest prostokątny.");
+        System.out.println("Wprowadź bok 1 trójkąta: ");
+        int side1 = sc.nextInt();
+        System.out.println("Wprowadź bok 2 trójkąta: ");
+        int side2 = sc.nextInt();
+        System.out.println("Wprowadź bok 3 trójkąta: ");
+        int side3 = sc.nextInt();
+
+        if (side1 < side2 + side3 && side2 < side1 + side3 && side3 < side1 + side2){
+            Triangle triangle = new Triangle(side1,side2,side3);
+            TriangleCalc triangleCalc = new TriangleCalc();
+
+            if (triangleCalc.isRightTriangle(triangle)){
+                System.out.println("Trójkąt jest prostokątny.");
+            } else {
+                System.out.println("Trójkąt nie jest prostokątny.");
+            }
         } else {
-            System.out.println("Trójkąt nie jest prostokątny.");
+            System.out.println("Podane długości boków nie utworzą trójkąta.");
         }
-
-
     }
 }
